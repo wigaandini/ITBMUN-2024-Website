a75c01c1123dc16689d04d453afe4fcfe66c653a
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-01-15 17:30:45 +0700

    changing fonts

--- itbmun-2024-web/src/pages/council.jsx
+++ itbmun-2024-web/src/pages/council.jsx
@@ -25,71 +25,74 @@ function Council() {
   };
 
   return (
-    <div className="CouncilList">
-      <h1> COUNCIL INFO </h1>
-      <Carousel
-        responsive={responsive}
-        autoPlay
-        autoPlaySpeed={5000}
-        transitionDuration={2000}
-        infinite={true}
-      >
-        <div className="CouncilCard">
-          <img src="/council/press-fix.png" alt="PRESS" />
-          <a
-            href="https://bestdelegate.com/a-guide-to-model-un-press-corps-committees/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2> PRESS </h2>
-          </a>
-          <h3> International Press Institute </h3>
-          {/* <p> Test description for PRESS council. </p> */}
+    <div className="Council">
+
+        <div className="CouncilList">
+        <h1> COUNCIL INFO </h1>
+        <Carousel
+            responsive={responsive}
+            autoPlay
+            autoPlaySpeed={5000}
+            transitionDuration={2000}
+            infinite={true}
+        >
+            <div className="CouncilCard">
+            <img src="/council/press-fix.png" alt="PRESS" />
+            <a
+                href="https://bestdelegate.com/a-guide-to-model-un-press-corps-committees/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <h2> PRESS </h2>
+            </a>
+            <h3> International Press Institute </h3>
+            {/* <p> Test description for PRESS council. </p> */}
+            </div>
+            <div className="CouncilCard">
+            <img src="/council/unesco-fix.png" alt="UNESCO" />
+            <a href="https://www.unesco.org/en">
+                <h2> UNESCO </h2>
+            </a>
+            <h3>
+                {" "}
+                United Nations Educational, Scientific and Cultural Organization{" "}
+            </h3>
+            {/* <p> Test description for UNESCO council. </p> */}
+            </div>
+            <div className="CouncilCard">
+            <img src="/council/crisis-fix.png" alt="CRISIS" />
+            <a href="https://bestdelegate.com/everything-you-need-to-know-about-crisis-committees/">
+                <h2> CRISIS </h2>
+            </a>
+            <h3> Crisis Council </h3>
+            {/* <p> Test description for CRISIS council. </p> */}
+            </div>
+            <div className="CouncilCard">
+            <img src="/council/cstd-fix.png" alt="CSTD" />
+            <a href="https://unctad.org/topic/commission-on-science-and-technology-for-development">
+                <h2> CSTD </h2>
+            </a>
+            <h3> Commission on Science and Technology for Development </h3>
+            {/* <p> Test description for CSTD council. </p> */}
+            </div>
+            <div className="CouncilCard">
+            <img src="/council/unodc-fix.png" alt="UNODC" />
+            <a href="https://www.unodc.org/">
+                <h2> UNODC </h2>
+            </a>
+            <h3> United Nations Office of Drugs and Crime </h3>
+            {/* <p> Test description for UNODC council. </p> */}
+            </div>
+            <div className="CouncilCard">
+            <img src="/council/unsc-fix.png" alt="UNSC" />
+            <a href="https://www.un.org/securitycouncil/">
+                <h2> UNSC </h2>
+            </a>
+            <h3> United Nations Security Council </h3>
+            {/* <p> Test description for UNSC council. </p> */}
+            </div>
+        </Carousel>
         </div>
-        <div className="CouncilCard">
-          <img src="/council/unesco-fix.png" alt="UNESCO" />
-          <a href="https://www.unesco.org/en">
-            <h2> UNESCO </h2>
-          </a>
-          <h3>
-            {" "}
-            United Nations Educational, Scientific and Cultural Organization{" "}
-          </h3>
-          {/* <p> Test description for UNESCO council. </p> */}
-        </div>
-        <div className="CouncilCard">
-          <img src="/council/crisis-fix.png" alt="CRISIS" />
-          <a href="https://bestdelegate.com/everything-you-need-to-know-about-crisis-committees/">
-            <h2> CRISIS </h2>
-          </a>
-          <h3> Crisis Council </h3>
-          {/* <p> Test description for CRISIS council. </p> */}
-        </div>
-        <div className="CouncilCard">
-          <img src="/council/cstd-fix.png" alt="CSTD" />
-          <a href="https://unctad.org/topic/commission-on-science-and-technology-for-development">
-            <h2> CSTD </h2>
-          </a>
-          <h3> Commission on Science and Technology for Development </h3>
-          {/* <p> Test description for CSTD council. </p> */}
-        </div>
-        <div className="CouncilCard">
-          <img src="/council/unodc-fix.png" alt="UNODC" />
-          <a href="https://www.unodc.org/">
-            <h2> UNODC </h2>
-          </a>
-          <h3> United Nations Office of Drugs and Crime </h3>
-          {/* <p> Test description for UNODC council. </p> */}
-        </div>
-        <div className="CouncilCard">
-          <img src="/council/unsc-fix.png" alt="UNSC" />
-          <a href="https://www.un.org/securitycouncil/">
-            <h2> UNSC </h2>
-          </a>
-          <h3> United Nations Security Council </h3>
-          {/* <p> Test description for UNSC council. </p> */}
-        </div>
-      </Carousel>
     </div>
   );
 }
