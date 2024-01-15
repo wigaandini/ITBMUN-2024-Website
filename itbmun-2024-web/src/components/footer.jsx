2ae026b0132ef8c3dc4612a6d5c4c3e405efe00e
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-01-16 01:42:09 +0700

    revising footer

--- itbmun-2024-web/src/components/footer.jsx
+++ itbmun-2024-web/src/components/footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
           <Instagram /> </a>
           <a href="mailto:itbmun2024@gmail.com">
           <EmailIcon /> </a>
-          <a href="https://www.linkedin.com/company/itbmun2024/">
+          <a href="https://www.linkedin.com/company/itbmodelunitednations">
           <LinkedIn /> </a>
         </p>
         <p className="Copyright"> &copy; ITBMUN 2024 </p>
