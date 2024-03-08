89fbd8c5c7beed87cdc46cb895ea9eeacf24bc4c
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-03-08 09:24:06 +0700

    revise

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,10 @@ import "../styles/about.css";
 function About() {
   return (
     <div className="about">
-      <div className="aboutTop">
+      <div
+        className="aboutTop"
+      ></div>
+      <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p>
           An annual series of events organized by its committee, led by a
@@ -14,27 +17,12 @@ function About() {
           June 2024!
         </p>
         <p>
-          Unfamiliar with MUNs? Don't worry – it's a <em><b> simulation of the{" "}
-          United Nations </b></em> where students act as delegates, addressing
-          real-world issues with the policies of their assigned countries. It's
-          the model of the United Nations. Whether you're passionate about MUN
-          or just curious, ITBMUN is the perfect place for you!
+          Unfamiliar with MUNs? Don't worry – it's a simulation of the United
+          Nations where students act as delegates, addressing real-world issues
+          with the policies of their assigned countries. It's the model of the
+          United Nations. Whether you're passionate about MUN or just curious,
+          ITBMUN is the perfect place for you!
         </p>
-      </div>
-      <div className="aboutBottom">
-        <p> Let's take a closer look at the previous ITBMUN 2023! </p>
-        <div className="video-container">
-          <iframe
-            title="Aftermovie ITBMUN 2023"
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/5w92xdfIaMA"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-          <p className="video-caption"><em> Aftermovie ITBMUN 2023 </em></p>
-          {/* harusnya tambahin dokum tp ntar ajalah mager */}
-        </div>
       </div>
     </div>
   );
