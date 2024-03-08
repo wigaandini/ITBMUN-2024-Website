a857f8a8a19546be24b100b0d345ca5cd8efdabd
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-03-08 19:51:12 +0700

    revising council logo

--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -3,7 +3,7 @@ import "../styles/registration.css";
 import Logo from "../assets/logo-black.png";
 
 function Registration() {
-  const GBURL = "*";
+  const GBURL = "https://drive.google.com/file/d/1G5iBCayiEid6e0tdKzoJiK42BLHIqdhC/view?usp=drivesdk";
   const CHAIRURL = "https://bit.ly/ChairRecruitmentITBMUN2024";
   const DELSINGLEURL = "https://docs.google.com/forms/d/e/1FAIpQLSdeQb3CRV30ttL9lD6w2M7j-_USdHFsLKLQCOI0xlZHfz26-Q/viewform?usp=sf_link";
   const DELURL = "https://docs.google.com/forms/d/e/1FAIpQLScHsdrSWmYjp4nczAdHPRjHJq2oSb1o5YqgsXna7hgbIFt9zA/viewform";
@@ -22,9 +22,9 @@ function Registration() {
       <div className="RegistrationForm">
         <div className="registrationContent">
           <p> What are you waiting for? Enroll immediately! </p>
-          <a href={CHAIRURL} target="_blank" rel="noopener noreferrer">
+          {/* <a href={CHAIRURL} target="_blank" rel="noopener noreferrer">
             <button> Chair </button>
-          </a>
+          </a> */}
           <a href={DELURL} target="_blank" rel="noopener noreferrer">
             <button> Delegations </button>
           </a>
