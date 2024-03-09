0f99eee26a0ee5f3c19a7a776314ff2989f2478c
Gulilil <juan.csantoso@gmail.com>
2024-03-09 22:30:51 +0700

    feat: finishing adjusting afmove iframe and responsiveness

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,26 +14,33 @@ function About() {
           June 2024!
         </p>
         <p>
-          Unfamiliar with MUNs? Don't worry – it's a <em><b> simulation of the{" "}
-          United Nations </b></em> where students act as delegates, addressing
-          real-world issues with the policies of their assigned countries. It's
-          the model of the United Nations. Whether you're passionate about MUN
-          or just curious, ITBMUN is the perfect place for you!
+          Unfamiliar with MUNs? Don't worry – it's a{" "}
+          <em>
+            <b> simulation of the United Nations </b>
+          </em>{" "}
+          where students act as delegates, addressing real-world issues with the
+          policies of their assigned countries. It's the model of the United
+          Nations. Whether you're passionate about MUN or just curious, ITBMUN
+          is the perfect place for you!
         </p>
       </div>
       <div className="aboutBottom">
         <p> Let's take a closer look at the previous ITBMUN 2023! </p>
         <div className="video-container">
-          <iframe
-            title="Aftermovie ITBMUN 2023"
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/5w92xdfIaMA"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-          <p className="video-caption"><em> Aftermovie ITBMUN 2023 </em></p>
-          {/* harusnya tambahin dokum tp ntar ajalah mager */}
+          <div className="whiteLayer"/>
+          <div className="iframeContainer">
+            <iframe
+              class="videoIframe"
+              title="Aftermovie ITBMUN 2023"
+              src="https://www.youtube.com/embed/5w92xdfIaMA"
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
+            {/* harusnya tambahin dokum tp ntar ajalah mager */}
+          </div>
+          <p className="video-caption">
+            <em> Aftermovie ITBMUN 2023 </em>
+          </p>
         </div>
       </div>
     </div>
